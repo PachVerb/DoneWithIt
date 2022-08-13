9ff8cb72faa21ab375710970826cab7af32e7c22
wangshan <22212@qq.com>
2022-08-13 19:13:27 +0800

    chore(engineer): init app dirtory

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+ * @LastEditTime: 2022-08-13 17:37:24
+ * @Description:
+ * @Date: 1985-10-26 16:15:00
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("app laughted");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello wolrd</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
